ac5d2daf1111d76bb2956dc157a39e4e27ea7d25
Viviane Puga <vnpuga@gmail.com>
2022-06-04 09:42:49 -0300

    requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,9 @@
+SELECT 
+	u.name AS "usuario",
+    COUNT(h.song_id) AS "qtde_musicas_ouvidas",
+    ROUND(SUM(s.duration / 60), 2) AS "total_minutos"
+FROM SpotifyClone.user AS u
+INNER JOIN SpotifyClone.history AS h ON h.user_id = u.user_id
+INNER JOIN SpotifyClone.song AS s ON s.song_id = h.song_id
+GROUP BY u.user_id
+ORDER BY u.name;
