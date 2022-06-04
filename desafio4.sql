87b07eff91577e6669fec73b180843d2ef0622d6
Viviane Puga <vnpuga@gmail.com>
2022-06-04 10:11:37 -0300

    requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+SELECT 
+	u.name AS "usuario",
+    IF(MAX(YEAR(h.reproduction_date)) >= 2021, "Usuário ativo", "Usuário inativo") AS "condicao_usuario"
+FROM SpotifyClone.user AS u
+INNER JOIN SpotifyClone.history AS h ON h.user_id = u.user_id
+GROUP BY u.user_id
+ORDER BY u.name;
